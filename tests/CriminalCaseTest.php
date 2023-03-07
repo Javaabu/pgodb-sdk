147803ab1f876e7b6c4f66aedae899293fbbaa88
ihshareef <ihshareef@users.noreply.github.com>
2023-03-07 12:32:20 +0000

    Fix styling

--- tests/CriminalCaseTest.php
+++ tests/CriminalCaseTest.php
@@ -67,7 +67,7 @@
 //});
 
 it('can filter and sort records', function () {
-    $db = new \Javaabu\PgoDB\PgoDB("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTdiN2Y5N2E0ODFjY2E2NDkwODAyZmZkNTUzYTJmNTEyZjA4OTU1ZWQwMjA3ODMxNmE1NDJiNDYwMDcwNDA3ZDM2MWM3YWI2OTg5NmVlODQiLCJpYXQiOjE2Nzc5OTY3OTAuMzg1NjMxLCJuYmYiOjE2Nzc5OTY3OTAuMzg1NjM2LCJleHAiOjE3MDk2MTkxOTAuMzIzNDc1LCJzdWIiOiIxIiwic2NvcGVzIjpbIioiXX0.Q6-7eF8Xq11QZjcAjLsLSlzNvnUy8HwEelz3BNtPyccPBkrwt-Z3oF7ICKK6XzXYVH35Wc-2v8_Y2UaIyf-U9V3NsIwl0hkKLWGwd-3nLdQ4XRbZJ_4oZp-SF2lIwT9sV-jawZMKLR5uT7OcR5SXbJitmIDUVSv5C_Dma7DrBY5WifmmjcCMNVXkUKWMxEObhFFfnHrmX3CdwskXdNZNrm2edPEby6AwT3TyyJ-xrAZyg2XJM1LD-gTohhtz7_yM-8VlrGdXEJSc78neCqEVVr79kKHBQ6LjLwra51ix8VLH_IPHoeH7mJtgpTOJpjLv77OJf9qu5SLt51cvHnX9k329w9-OJkobdowRJRMkRnAPr7SIvuvvFGBZSARwvyWOLbreseBLUlrbtOPXU21EFBpHt9jmI92xTFJgmvtZt-r_yIgUq3O2pmR_slny1ylmeMo1j0YX-_elzxDvD8X5E4Z-X86IHPgFlmiXz6gOOfKBLrSWAlCrSEZhys7xFrHWup_F9J3rRcXgVnTvF8RJRp2gDcYCnFwng0mfgcxouNnX_M2Bv00MmOrLPk7GMGUPXe-bqaRWhcArGED9VjPpqjKQVpAZyiB7uIE3-fCxBv3g9iwLJSrXVAZ_e0BJBLDt0VyUuZYpMRb9dR63ofLZf-_Y5bhXriGsILrXiLG8lIA");
+    $db = new \Javaabu\PgoDB\PgoDB('eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTdiN2Y5N2E0ODFjY2E2NDkwODAyZmZkNTUzYTJmNTEyZjA4OTU1ZWQwMjA3ODMxNmE1NDJiNDYwMDcwNDA3ZDM2MWM3YWI2OTg5NmVlODQiLCJpYXQiOjE2Nzc5OTY3OTAuMzg1NjMxLCJuYmYiOjE2Nzc5OTY3OTAuMzg1NjM2LCJleHAiOjE3MDk2MTkxOTAuMzIzNDc1LCJzdWIiOiIxIiwic2NvcGVzIjpbIioiXX0.Q6-7eF8Xq11QZjcAjLsLSlzNvnUy8HwEelz3BNtPyccPBkrwt-Z3oF7ICKK6XzXYVH35Wc-2v8_Y2UaIyf-U9V3NsIwl0hkKLWGwd-3nLdQ4XRbZJ_4oZp-SF2lIwT9sV-jawZMKLR5uT7OcR5SXbJitmIDUVSv5C_Dma7DrBY5WifmmjcCMNVXkUKWMxEObhFFfnHrmX3CdwskXdNZNrm2edPEby6AwT3TyyJ-xrAZyg2XJM1LD-gTohhtz7_yM-8VlrGdXEJSc78neCqEVVr79kKHBQ6LjLwra51ix8VLH_IPHoeH7mJtgpTOJpjLv77OJf9qu5SLt51cvHnX9k329w9-OJkobdowRJRMkRnAPr7SIvuvvFGBZSARwvyWOLbreseBLUlrbtOPXU21EFBpHt9jmI92xTFJgmvtZt-r_yIgUq3O2pmR_slny1ylmeMo1j0YX-_elzxDvD8X5E4Z-X86IHPgFlmiXz6gOOfKBLrSWAlCrSEZhys7xFrHWup_F9J3rRcXgVnTvF8RJRp2gDcYCnFwng0mfgcxouNnX_M2Bv00MmOrLPk7GMGUPXe-bqaRWhcArGED9VjPpqjKQVpAZyiB7uIE3-fCxBv3g9iwLJSrXVAZ_e0BJBLDt0VyUuZYpMRb9dR63ofLZf-_Y5bhXriGsILrXiLG8lIA');
 //
 //    $item1 = [
 //        'incident_reference_number' => '113',
@@ -82,12 +82,10 @@ it('can filter and sort records', function () {
 //        'incident_at' => '1994-12-30T09:11:26.000000Z',
 //        'lodged_at' => '1998-03-13T19:00:00.000000Z',
 //    ];
-////
+    ////
 //    $db->criminalCase()->store($item1);
 //    $db->criminalCase()->store($item2);
 
     // sort records
-    $defaultArray = $db->criminalCase()->addSort('incident_at')->addFilter('search','ABC','A079092')->get();
-
-
+    $defaultArray = $db->criminalCase()->addSort('incident_at')->addFilter('search', 'ABC', 'A079092')->get();
 });
